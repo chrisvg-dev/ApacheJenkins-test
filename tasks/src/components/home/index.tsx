0e657e90587f65fdf56243edbd3f76d688afb740
Cristhian Villegas Garcia <cristianvg9692@gmail.com>
2023-04-03 22:59:45 -0600

    Primera version Jenkins

---
+++ tasks/src/components/home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const App = () => {
+    return (
+        <div className='container'>
+            <h1 className='text text-secondary'>Hello</h1>
+            <input type="text" className='form-control' />
+        </div>
+    );
+}
