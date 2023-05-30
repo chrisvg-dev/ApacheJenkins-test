0aaae15615c9708dbaec0decf047d899f58d4da9
Cristhian Villegas <cristianvg9692@gmail.com>
2023-05-29 22:36:12 -0600

    cdsacsdacsad

--- tasks/src/components/home/index.tsx
+++ tasks/src/components/home/index.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export const App = () => {
+    const [ repositories, setRepositories ] = useState<any[]>([]);
+
+    const fetchPost = async () => {
+        await fetch('https://api.github.com/users/chrisvg-dev/repos')
+            .then(data => data.json())
+            .then( data => {
+                setRepositories(data)
+            } );
+    };
+
     return (
-        <div className='container'>
-            <h1 className='text text-secondary'>Hello</h1>
-            <input type="text" className='form-control' />
-        </div>
+        <>
+            <div className='container'>
+                <h1 className='text text-secondary'>Hello</h1>
+                <input type="text" className='form-control' />
+                <ul>
+                    { repositories.map((data: any) =>
+                        {
+                            return <li key={data.id}>                                
+                                {data.full_name}
+                            </li>
+                        }
+                    )}
+                </ul>
+                <button onClick={fetchPost}>Hello</button>
+            </div>
+        </>
     );
 }
