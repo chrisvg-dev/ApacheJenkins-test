253b02fd1d2def46ed544286b5eb92791c62485f
Abraham Morales <abraham.morales.hueto@gmail.com>
2023-05-29 23:07:05 -0600

    CAMBIO

--- tasks/src/components/home/index.tsx
+++ tasks/src/components/home/index.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
-export const App = () => {
+export const App = ():any => {
     const [ repositories, setRepositories ] = useState<any[]>([]);
 
     const fetchPost = async () => {
